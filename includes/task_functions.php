13195455d876dd35936e50eac4f5ff819a6fb302
Nazim6269 <nazimdev10022001@gmail.com>
2026-08-25 12:25:57 +0600

    feat: add getTasks() function to read tasks from JSON file

--- includes/task_functions.php
+++ includes/task_functions.php
@@ -1,1 +1,25 @@
 <?php
+
+
+function getTasks(string $filePath): array
+{
+
+  if (!file_exists($filePath)) {
+
+    return [];
+  }
+
+  $jsonContent = file_get_contents($filePath);
+
+  if ($jsonContent == false || $jsonContent == '') {
+    return [];
+  }
+
+  $tasks = json_decode($jsonContent, true);
+
+  if (!is_array($tasks)) {
+    return [];
+  }
+
+  return $tasks;
+}
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
 require_once 'includes/task_functions.php';
 
+$filePath = 'data/task.json';
+$tasks = getTasks($filePath);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +16,7 @@ require_once 'includes/task_functions.php';
 <body>
 
   <h1>My To Do List</h1>
-
+  <pre> <?php print_r($tasks); ?> </pre>
 </body>
 
 
