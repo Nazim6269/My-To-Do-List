5fbc3527f122ff430c4c42251fecda0a26e82554
Nazim6269 <nazimdev10022001@gmail.com>
2026-08-25 12:15:02 +0600

    chore: set up initial project structure for to-do list

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'includes/task_functions.php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>To DO List</title>
+</head>
+
+<body>
+
+  <h1>My To Do List</h1>
+
+</body>
+
+
+</html>
